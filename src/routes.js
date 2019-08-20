e6521c239b20d6211d59c7bd697a0c5715dbde44
Adriano Chambel <acmlima.softweb@gmail.com>
2019-08-20 18:36:17 +0100

    Added User model

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,17 @@
 import { Router } from 'express';
 
+import User from './app/models/User';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({
-    message: 'Hello world!',
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'John Snow',
+    email: 'john.snow@domain.com',
+    password_hash: '123456',
   });
+
+  return res.json(user);
 });
 
 export default routes;
